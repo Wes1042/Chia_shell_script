430f4219cb4eff3521e032b30c11b64bc026fbf1
Wes1042 <wesleytrujillo@uagateway.org>
2021-11-10 22:33:15 -0500

    VErsion 1

---
+++ chia_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# creates a function that generates the chia command from # can generate multiple options for k values . Ex . If space = a number in range of k value then generate 
+Chia_command()   #Returns command and number that you choose that can run with the parallel plot 
+#find_cpu , find_ram , findspace, 
+
+
+# finds the cpu of the device 
+find_cpu () 
+{
+processor = grep -c processors
+echo 'You have $processor processors'
+threads = grep -c threads /proc/cpuinfo
+echo 'You have $threads threads'
+}
+
+
+# finds the total amount of Ram 
+Find_Ram() 
+ram = | free -h
+
+Find_space()
+# Find total amount of space there is and display how much space you have left which will calculate how much space we can use 
+
+Parallel_plotting(chia_command)
+# if user typed yes then the program will run in the command with multiple shells . Will open other shells 
+Maybe open screen 
+
+Check_venv()
+# if user is not in venv mode (will have trouble figuring this out ) activate the block chain activate directory  and run 
+
+Set_directories() 
+# sets the temporary directory and final destination 
+
+Get_input()
+# gets input of the user and can be used multiple times

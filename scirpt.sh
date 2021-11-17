29004d4ff3b5ba79940c333cf29d7594f3a7ca2e
Wes1042 <wesleytrujillo@uagateway.org>
2021-11-17 13:48:47 -0500

    45% done

--- scirpt.sh
+++ scirpt.sh
@@ -5,22 +5,19 @@ Chia_command(){
 }
 
 find_cpu(){
-
+# Finds cpu & amount of threads and will hold them in variables for later use
 Cores=$(( $(lscpu | awk '/^Socket\(s\)/{ print $2 }') * $(lscpu | awk '/^Core\(s\) per socket/{ print $4 }') ))
 Threads_per_core=$(( $(lscpu |awk '/^Thread\(s\) per core/{ print $4 }') ))
 Threads=$(($Cores * $Threads_per_core))
 echo "Cores   = $Cores" ; echo "Threads = $Threads"
 
-
 }
 
 find_memory(){
-
+# Finds memeory in KB
 mmemory=($(grep MemTotal /proc/meminfo |awk '{ print $2}'))
 echo "This device has $memory KB of memory"
-
 echo -e "\nKB\t\t\tMB\t\t\tGB\t\tTB"
-
 
 for (( i=0; i<=3; i++ ))
 do
@@ -28,8 +25,7 @@ do
   memory=$(echo "scale=3;$memory/1024" |bc)
 done
 
-
-
+# Finds memory in KB and converts to KiB 
 memoryib=($(grep MemTotal /proc/meminfo |awk '{ print $2}'))
 memoryib=$(echo "scale=3;$memoryib/1.024" |bc )
 echo -e "\n\nThis device has $memoryib KiB of memory"
@@ -45,5 +41,6 @@ echo -e "\n\n"
 }
 
 find_space(){
-
+lsblk -o name,size,mountpoint -lpn |grep "/dev/sd"
+# perhaps have a input where it holds where you want to hold the temp and plots folder
 }

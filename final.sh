ec5e33ff78f1d47fe5e116734d88d9bf25d2ae8b
Wes1042 <wesleytrujillo@uagateway.org>
2021-11-30 17:20:31 -0500

    95% , now we add the rest

--- final.sh
+++ final.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Created global variables that every function can use. Avoid the unnecessary oytput information. 
 
 # generate chia commands based on resources
 chia_generator(){
@@ -16,82 +16,84 @@ find_cpu_threads(){
     threads_per_core=$(( $(lscpu |awk '/^Thread\(s\) per core/{ print $4 }') ))
     threads=( "$cores" * "$threads_per_core")
     # threads= (Sockets X Cores per socket) X threads_per_core 
-    echo "$threads" 
+    #echo "$threads" 
 }
+threads=0
+find_cpu_threads
 
+    
 plotting_threads(){
-    plots=("$plots_per_space")
-    echo "hahahah $plots"
-    threads=("$find_cpu_threads")
-
-    #plotting_threads=$(echo "$threads"  "$plots" |awk '{print $2 / $1}')
-    plotting_threads=( "$plots" / "$threads" )
-    echo "$plotting_threads"
-
+    plotting_threads=$(echo "$threads"  "$plots" |awk '{print $1 / $2}')
+    #echo "$plotting_threads"
 }
+plotting_threads=0
+
+
 # Returns total memory
 find_total_memory(){
     memory=("$(grep MemTotal /proc/meminfo |awk '{ print $2}')")
     total_memory=$(echo "scale=3;$memory/1024"|bc )
-
-    echo "$total_memory"
+    #echo "$total_memory"
 }
+total_memory=0
+find_total_memory
+echo $total_memory
 
 # Return memory that can be divided based on plots
 plotting_memory(){
-    total_memory=$(find_total_memory)
-    plots=$(plots_per_space)
     plotting_memory=$(echo "$total_memory $plots" | awk '{print $1 / $2}')
-    #plotting_memory=$(echo "scale=3; $total_memory / $plots" |bc)
-    echo "$plotting_memory"
+    #echo "$plotting_memory"
 }
-
-# Returns total _space
-find_total_space(){
-    total_space=$(df /mnt/chia/ |grep dev |awk '{print $2}')
-
-    echo "$total_space"
-}
+plotting_memory=0
 
 #Finds amount of plots based on space size
 plots_per_space(){
-    space=$(available_space)
+    
     #available_space=$(df /mnt/chia/ |grep dev |awk '{print $3}')
     #space=$(("$available_space" /1024 |bc  ))
     plots=$(("$space"/365500)) 
 
-    echo $plots
+    #echo $plots
 }
-
-
+plots=0
 
 #Finds available space based on drive location
 available_space(){
-    available_space=$(df /mnt/chia/ |grep dev |awk '{print $3}')
-    space=$(("$available_space" /1024 |bc  ))
+    available_space=$(df /mnt/chia/ |grep dev |awk '{print $4}')
+    space=$(("$available_space" /1024  ))
 
-    echo $space
+    #echo $space
 
 }
+space=0
+available_space
 # Returns temporary chia directory
 temp_location(){
     temp="/mnt/chia/temp"
-    echo $temp
+    #echo "$temp"
 }
+temp=""
+temp_location
 # Returns destination chia directory
 dest_location(){
     final="/mnt/chia/plots"
-    echo $final
+    #echo $final
 }
+final=""
+dest_location 
 
-find_total_memory
-available_space
 plots_per_space
 plotting_threads
 plotting_memory
 
+outputs(){
+    echo "You have a total of $space Gb available space"
+    echo "You can create $plots plots based on your available space"
+    echo "This is your command: "
+    command=$(chia_generator "$plotting_memory" "$plotting_threads" "$temp" "$final") 
+    echo $command
+}
 
-temp_location
-dest_location
+outputs
 
-chia_generator "$plotting_memory" "$plotting_threads" "$temp_location" "$dest_location"
+#chia_generator "$plotting_memory" "$plotting_threads" "$temp" "$final"
